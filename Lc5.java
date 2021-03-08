e38af9ae8665db26421bef584ecfb6b9910df785
Kuma <kumaj@qq.com>
2021-03-08 20:54:18 +0800

    更新记忆化解法，但超时！

--- Lc5.java
+++ Lc5.java
@@ -6,26 +6,33 @@ package leetcode;
  * 5. 最长回文子串
  */
 public class Lc5 {
+    int[][] bp;
     public String longestPalindrome(String s) {
-        boolean[][] bp = new boolean[s.length()][s.length()];
+        int n = s.length();
+        bp = new int[n][n];
         String res = "";
-//        字串长度
-        for (int i = 0; i < s.length(); i++) {
-//            起始位置
-            for (int j = 0; i + j < s.length(); j++) {
-                int k = i + j;
-                if (i == 0){
-                    bp[j][k] = true;
-                }else if (i == 1 && s.charAt(j) == s.charAt(k)){
-                    bp[j][k] = true;
-                }else {
-                    bp[j][k] = bp[j + 1][k - 1] && s.charAt(j) == s.charAt(k);
-                }
-                if (bp[j][k] && res.length() < i + 1){
-                    res = s.substring(j, k + 1);
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (m(s, i, j) > 0 && res.length() < j - i + 1){
+                    res = s.substring(i, j + 1);
                 }
             }
         }
         return res;
     }
+//    记忆化
+    public int m(String s, int i, int j){
+        if (bp[i][j] != 0){
+            return bp[i][j];
+        }
+        if (i >= j){
+            bp[i][j] = 1;
+        }else if (s.charAt(i) == s.charAt(j)){
+            return m(s, i + 1, j - 1);
+        }else {
+            bp[i][j] = -1;
+        }
+        return bp[i][j];
+    }
+
 }
