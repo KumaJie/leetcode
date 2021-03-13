2863cbe0b800c548401b9682e1aa2bd33d016284
Kuma <kumaj@qq.com>
2021-03-13 20:16:00 +0800

    二叉树性质解法

--- Lc331.java
+++ Lc331.java
@@ -9,24 +9,19 @@ import java.util.Stack;
  */
 public class Lc331 {
     public boolean isValidSerialization(String preorder) {
-        Stack<String> st = new Stack<>();
-        String[] pre = preorder.split(",");
-        int n = pre.length;
-        int i = 0;
-        while (i < n){
-            String str = pre[i];
-            if (!st.isEmpty() && "#".equals(str) && "#".equals(st.peek())) {
-                while (!st.isEmpty() && "#".equals(st.peek())) {
-                    st.pop();
-                    if (st.isEmpty()) {
-                        return false;
-                    }
-                    st.pop();
-                }
+//        根节点入度0，出度2
+        int dif = -1;
+        for (String s : preorder.split(",")){
+//                空节点入度1，出度0
+            dif += 1;
+            if (dif > 0){
+                return false;
             }
-            st.push(str);
-            i++;
+            if (!"#".equals(s)){
+//                普通节点入度1，出度2
+                dif -= 2;
+            }
         }
-        return st.size() == 1 && "#".equals(st.peek());
+        return dif == 0;
     }
 }
