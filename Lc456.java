ee2bd2872c67c92cf4f4eb30997c17101071c5c5
Kuma <kumaj@qq.com>
2021-03-24 20:30:28 +0800

    456.132 模式

--- Lc456.java
+++ Lc456.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 /**
@@ -9,14 +10,22 @@ import java.util.Stack;
  */
 public class Lc456 {
     public boolean find132pattern(int[] nums) {
-        int min = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            for (int j = nums.length - 1; j > i; j--) {
-                if (min < nums[j] && nums[j] < nums[i]){
-                    return true;
-                }
+        int n = nums.length;
+        int[] min = new int[n];
+        Arrays.fill(min,Integer.MAX_VALUE);
+        for (int i = 1; i < n; i++) {
+            min[i] = Math.min(min[i - 1], nums[i - 1]);
+        }
+        Stack<Integer> st = new Stack<>();
+        for (int i = n - 1; i > 0; i--) {
+            int minTmp = Integer.MIN_VALUE;
+            while (!st.isEmpty() && st.peek() < nums[i]){
+                minTmp = st.pop();
             }
-            min = Math.min(min, nums[i]);
+            if (minTmp > min[i]){
+                return true;
+            }
+            st.push(nums[i]);
         }
         return false;
     }
